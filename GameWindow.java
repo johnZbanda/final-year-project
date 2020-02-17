a56b6aaf230a89903fe426c7a2bf4b1edb46ea69
Banda <sgjbanda@liverpool.ac.uk>
2020-02-17 16:07:43 +0000

    buttons appear on screen for all difficulties. add functionality to buttons. to do: quit button, new game button, database stuff

--- GameWindow.java
+++ GameWindow.java
@@ -1,40 +1,58 @@
-import java.awt.*;
 import java.awt.event.*; //For ActionListener
 import javax.swing.*; //Using Java Spring for 
 
 public class GameWindow extends JFrame implements ActionListener{
     //UI for the game
     Minesweeper game;
-
+    static int newDimensions;
     public GameWindow(int difficulty) {
+        int x = 1;
+        int y = 1;
         setLayout(null);
+        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         game = new Minesweeper(); //needed
         game.chooseDifficulty(difficulty);
-
+        newDimensions = game.dimensions;
         JPanel panel = new JPanel();
-        JButton button[] = new JButton[game.dimensions * game.dimensions]; //initialise buttons
-        panel.setLayout(new GridLayout(game.dimensions, game.dimensions));
-        panel.setBorder(BorderFactory.createLineBorder(Color.gray, 3));
-        panel.setBackground(Color.white);
+        JButton button[] = new JButton[newDimensions * newDimensions]; //initialise buttons
 
-        for (int i = 0; i < (game.dimensions * game.dimensions); i++) {
+        for (int i = 0; i < (newDimensions * newDimensions); i++) {
             button[i] = new JButton(); //NullPointerException
-            panel.add(button[i]);    
+            if (x == newDimensions) {
+                button[i].addActionListener(this);
+                button[i].setBounds(35 * x, 35 * y, 30, 30);
+                button[i].setVisible(true);
+                super.add(button[i]);
+                x = 1;
+                y++;
+            } else {
+                button[i].addActionListener(this);
+                button[i].setBounds(35 * x, 35 * y, 30, 30);
+                button[i].setVisible(true);
+                super.add(button[i]);
+                x++;
+            }
         }
-
-        super.getContentPane().add(panel);
-        super.pack();
         
         super.setTitle("Minesweeper");
-        super.setSize(900, 600);
-        super.setResizable(false);
-        super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        switch (difficulty) {
+            case 1:
+                super.setSize(400,500);
+                break;
+            case 2:
+                super.setSize(650,700);
+                break;
+            case 3:
+                super.setSize(920,980);
+                break;
+        }
+        super.setResizable(true);
         super.setVisible(true);
     }
     
 
     public void actionPerformed(ActionEvent e) {
-
+        
     }
 }
